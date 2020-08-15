46d2315e83b4e562641f32ffbcc93a2637fdf65b
Aditya Chavan <adityachavan677@gmail.com>
2020-08-15 15:48:56 +0530

    made changes in routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route } from "react-router-dom"; // .
 
 import ArticleList from "./containers/ArticleListView";
 import ArticleDetail from "./containers/ArticleDetailView";
